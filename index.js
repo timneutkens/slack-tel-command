10b1cda92f1841c604c954aa1a59931f71e8a651
Albin Ekblom <albinekblom@gmail.com>
2016-09-30 10:16:50 +0200

    🌹 Add standard

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 const pify = require('pify')
-const { json, send, createError } = require('micro')
-const { parse } = require('querystring')
-const getRawBody = require('raw-body')
-const typer = require('media-typer')
 const parseFormData = require('urlencoded-body-parser')
 const list = pify(require('slack/methods/users.list'))
 const Fuse = require('fuse.js')
 
-async function fetchUsers() {
-    const { members } = await list({token: process.env.SLACK_API_TOKEN})
-    return members.reduce((users, { name, real_name: realName, deleted, is_bot: isBot, profile: {phone} }) => {
-      if(deleted || isBot || !phone) {
-        return users
-      }
+async function fetchUsers () {
+  const { members } = await list({token: process.env.SLACK_API_TOKEN})
+  return members.reduce((users, { name, real_name: realName, deleted, is_bot: isBot, profile: {phone} }) => {
+    if (deleted || isBot || !phone) {
+      return users
+    }
 
-      return [...users, { name: realName || name, phone }]
-    }, [])
+    return [...users, { name: realName || name, phone }]
+  }, [])
 }
 
-function renderList(data) {
+function renderList (data) {
   return data.map(({name, phone}) => {
     return `*${name}*: ${phone}`
-  }).join("\n")
+  }).join('\n')
 }
 
-module.exports = async function(req, res) {
-  if(req.method !== 'POST') {
+module.exports = async function (req, res) {
+  if (req.method !== 'POST') {
     return ''
   }
 
   const {text: commandText, token} = await parseFormData(req)
-  if(token !== process.env.SLACK_TOKEN) {
+  if (token !== process.env.SLACK_TOKEN) {
     return ''
   }
 
@@ -42,20 +38,20 @@ module.exports = async function(req, res) {
     distance: 100,
     maxPatternLength: 32,
     keys: [
-      "name",
-      "phone"
+      'name',
+      'phone'
     ]
   })
 
   const searchText = commandText || ''
 
-  if(searchText === 'all') {
+  if (searchText === 'all') {
     return renderList(data)
   }
 
   const results = fuse.search(searchText)
 
-  if(results.length > 0) {
+  if (results.length > 0) {
     return renderList(results)
   }
 
